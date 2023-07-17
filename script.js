895c72ec386db9a34fb90136c85d1508596c4849
umesh jyani <49265224+Umeshjyani@users.noreply.github.com>
2023-07-17 07:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let line=document.querySelector("#line");
 let angle=0;
 function rotate() {
 	angle+=1;
+	if(angle==360)
+		angle=0;
 	line.style.transform=`rotate(${angle}deg)`
 }
 setInterval(rotate,0);
